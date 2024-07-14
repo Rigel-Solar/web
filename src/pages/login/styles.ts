581847f2ca5b893e4c565d4d79ca3b572e70f6d6
cleitinsz <gabriel.playhard10@gmail>
2024-07-14 11:37:42 -0300

    style(Login): fix login scroll

--- src/pages/login/styles.ts
+++ src/pages/login/styles.ts
@@ -4,7 +4,6 @@ export const Container = styled.div`
 	display: grid;
 	grid-template-columns: 1fr 1fr;
 	width: 100%;
-	height: 100vh;
 	background-color: ${({ theme }) => theme.colors.brand.background};
 
 	@media (max-width: 960px) {
