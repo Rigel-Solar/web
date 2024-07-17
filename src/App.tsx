02178f6690ad5573731b079928d746837bf99ea7
cleitinsz <gabriel.playhard10@gmail>
2024-07-17 20:13:25 -0300

    feat(persist): persist token on redux persist

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 import { Toaster } from "sonner";
 import { ThemeProvider } from "styled-components";
-import store from "./redux/store";
+import Loading from "./components/loading";
+import { persistor, store } from "./redux/store";
 import { MainRoutes } from "./routes/MainRoutes";
 import GlobalStyle from "./styles/globalStyle";
 import { theme } from "./styles/themes";
@@ -13,24 +15,26 @@ export default function App() {
 	return (
 		<ThemeProvider theme={theme}>
 			<Provider store={store}>
-				<QueryClientProvider client={queryClient}>
-					<BrowserRouter>
-						<GlobalStyle />
-						<Toaster
-							richColors
-							position="top-right"
-							duration={3000}
-							toastOptions={{
-								style: {
-									height: 50,
-									padding: 20,
-									whiteSpace: "pre-line",
-								},
-							}}
-						/>
-						<MainRoutes />
-					</BrowserRouter>
-				</QueryClientProvider>
+				<PersistGate loading={<Loading />} persistor={persistor}>
+					<QueryClientProvider client={queryClient}>
+						<BrowserRouter>
+							<GlobalStyle />
+							<Toaster
+								richColors
+								position="top-right"
+								duration={3000}
+								toastOptions={{
+									style: {
+										height: 50,
+										padding: 20,
+										whiteSpace: "pre-line",
+									},
+								}}
+							/>
+							<MainRoutes />
+						</BrowserRouter>
+					</QueryClientProvider>
+				</PersistGate>
 			</Provider>
 		</ThemeProvider>
 	);
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,40 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+	FLUSH,
+	PAUSE,
+	PERSIST,
+	persistReducer,
+	persistStore,
+	PURGE,
+	REGISTER,
+	REHYDRATE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer from "./reducers/user-reducer";
 
-const store = configureStore({
-	reducer: {
-		user: userReducer,
-	},
+const rootReducer = combineReducers({
+	user: userReducer,
 });
 
+const persistConfig = {
+	key: "root",
+	storage: storage,
+	whitelist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
+});
+
+export const persistor = persistStore(store);
+
 export type RootState = ReturnType<typeof store.getState>;
-
 export type AppDispatch = typeof store.dispatch;
-
-export default store;
